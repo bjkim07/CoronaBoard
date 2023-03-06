711c3deabf38e584dae0e7389323450b58a12f35
bjkim07 <ziocck@gmail.com>
2023-03-06 16:03:43 +0900

    230306_study

--- coronaboard-web/src/templates/single-page.js
+++ coronaboard-web/src/templates/single-page.js
@@ -3,15 +3,16 @@ import { Slide } from '../components/slide';
 
 export default function SinglePage({ pageContext }) {
     const { dataSource } = pageContext;
-    const { thirdSlideTitle } = dataSource;
+    const { countryByCc, globalStats } = dataSource;
+
+    // const { thirdSlideTitle } = dataSource;
+    console.log(countryByCc);
+    console.log(globalStats);
 
     return(
         <div>
             <h1>코로나보드 </h1>
-            <p>createPage로 만들어진 페이지 입니다</p>
             <Slide title="국가별 현황">국가별 현황을 보여줍니다.</Slide>
-            <Slide title={ '대한민국 지역별 현황'}>대한민국 지역별 현황을 보여줍니다</Slide>
-            <Slide title={ thirdSlideTitle }>예방행동 수칙을 보여줍니다.</Slide>
         </div>
     );
 }
