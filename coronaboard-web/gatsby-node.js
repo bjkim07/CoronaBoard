23cf40bc5f2837ebb349d4904d734ba8d25d2b21
bjkim07 <ziocck@gmail.com>
2023-03-09 09:36:55 +0900

    230309_study

--- coronaboard-web/gatsby-node.js
+++ coronaboard-web/gatsby-node.js
@@ -1,10 +1,10 @@
-const {getDataSource} = require('./src/data-loader');
+// const {getDataSource} = require('./src/data-loader');
 
 exports.createPages = async ({ actions }) => {
     const { createPage } = actions;
-    const dataSource = await getDataSource();
+    // const dataSource = await getDataSource();
 
-    // const dataSource = { thirdSlideTitle: '예방 행동 수칙'};
+    const dataSource = { thirdSlideTitle: '예방 행동 수칙'};
 
     createPage({
         path: '/',
--- coronaboard-web/src/pages/bootstrap/index.js
+++ coronaboard-web/src/pages/bootstrap/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 export default function BootstrapPage() {
     return <div>코로나보드!</div>;
 }
